e20654798310868d7d8daf749e77e5e1f2b16ac5
Gustavo Adolfo Gómez Burgos <gustcadc@gmail.com>
2026-03-17 13:27:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,7 @@ if ingredients_list:
 
 #New section to display smoothierfroot nutrition information
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
