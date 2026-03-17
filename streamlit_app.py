d027aa81a295eb587707258bbbfab0fb49573b87
Gustavo Adolfo Gómez Burgos <gustcadc@gmail.com>
2026-03-17 13:03:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
+#New section to display smoothierfroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 #st.text(smoothiefroot_response.json())
